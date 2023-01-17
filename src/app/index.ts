34585404c31ec5028c5516b7bd0ffdb83c2186f3
hoff <ie@fv.ee>
2023-01-17 08:31:30 +0300

    init project

---
+++ src/app/index.ts
@@ -0,0 +1,3 @@
+// Your code
+// eslint-disable-next-line max-len
+// https://medium.com/fafnur/%D1%81%D0%B1%D0%BE%D1%80%D0%BA%D0%B0-typescript-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F-%D1%81-%D0%BF%D0%BE%D0%BC%D0%BE%D1%89%D1%8C%D1%8E-webpack-8cd04eba6a8f
